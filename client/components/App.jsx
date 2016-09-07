909db457c91da33af911adf22bb54bde5eaca3c0
Devon McGrath <devon.mcgrath@gmail.com>
2016-09-07 21:22:44 +1200

    Initial Commit

---
+++ client/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default React.createClass({
+  render() {
+    return (
+        <div>
+          <p>Hello World</p>
+        </div>
+    )
+  }
+})
---
+++ readme.md
@@ -0,0 +1,9 @@
+#React Starter project
+
+#Setup
+
+* clone the repo
+* npm install
+* npm run build
+* npm run start
+running on localhost:8080
